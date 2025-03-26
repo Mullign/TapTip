6b10758d7e728a7eef7227d068b36e2595738b46
Mull1gan <russellproductiongroup@gmail.com>
2025-03-26 08:08:36 -0400

    Initial Commit

---
+++ TapTip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TapTip
+//
+//  Created by No Name Society  on 3/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TapTip/TapTipApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapTipApp.swift
+//  TapTip
+//
+//  Created by No Name Society  on 3/26/25.
+//
+
+import SwiftUI
+
+@main
+struct TapTipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
